c409e39e95bd6a5df9701dfb0a25d38f5e24fa64
Vic <vikktimk@yandex.ru>
2025-05-26 20:41:11 +0300

    Arrays

--- Arrays/Arrays/obj/Debug/net8.0/Arrays.AssemblyInfo.cs
+++ Arrays/Arrays/obj/Debug/net8.0/Arrays.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstApp")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f847de726f45b1767be68df212b03be4e88a977b")]
-[assembly: System.Reflection.AssemblyProductAttribute("MyFirstApp")]
-[assembly: System.Reflection.AssemblyTitleAttribute("MyFirstApp")]
+[assembly: System.Reflection.AssemblyProductAttribute("Arrays")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Arrays")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
