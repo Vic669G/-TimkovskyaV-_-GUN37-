4d4c77115aec71ae20bd1a4eba36a3ee7ab753b6
Vic <vikktimk@yandex.ru>
2025-08-31 14:22:44 +0300

    todo comment

---
+++ Assets/Scripts/EditorMover.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+	
+	[RequireComponent(typeof(PositionSaver))]
+	public class EditorMover : MonoBehaviour
+	{
+		private PositionSaver _save;
+		private float _currentDelay;
+		
+		//todo comment: Что произойдёт, если _delay > _duration?
+		//Эффект может не успеть начаться, поэтому мы его не увидим.
+		private float _delay = 0.5f;
+		private float _duration = 5f;
+
+		private void Start()
+		{
+            //todo comment: Почему этот поиск производится здесь, а не в начале метода Update?
+            //Поиск _save и очистака его данных делаются в Start, потому что это инициализация объекта. В Update это бы затрудняло производительность и нарушало работу логики. 
+            _save = GetComponent<PositionSaver>();
+			_save.Records.Clear();
+		}
+
+		private void Update()
+		{
+			_duration -= Time.deltaTime;
+			if (_duration <= 0f)
+			{
+				enabled = false;
+				Debug.Log($"<b>{name}</b> finished", this);
+				return;
+			}
+			
+			//todo comment: Почему не написать (_delay -= Time.deltaTime;) по аналогии с полем _duration?
+			//Может возникнуть путаница: эффект начнётся раньше или позже, или _delay уйдёт в минус и будет мешать вычислениям.
+			_currentDelay -= Time.deltaTime;
+			if (_currentDelay <= 0f)
+			{
+				_currentDelay = _delay;
+				_save.Records.Add(new PositionSaver.Data
+				{
+					Position = transform.position,
+					//todo comment: Для чего сохраняется значение игрового времени?
+					//Чтобы отсчитывать задержки или сроки действия, сохранения момента событий, синхронизации процессов и управление эффектами.
+					Time = Time.time,
+				});
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/PositionSaver.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+	public class PositionSaver : MonoBehaviour
+	{
+		public struct Data
+		{
+			public Vector3 Position;
+			public float Time;
+		}
+
+		private TextAsset _json;
+
+		public List<Data> Records { get; private set; }
+
+		private void Awake()
+		{
+			//todo comment: Что будет, если в теле этого условия не сделать выход из метода?
+			//Игра может упасть или скрипт перестанет работать.
+			if (_json == null)
+			{
+				gameObject.SetActive(false);
+				Debug.LogError("Please, create TextAsset and add in field _json");
+				return;
+			}
+			
+			JsonUtility.FromJsonOverwrite(_json.text, this);
+			//todo comment: Для чего нужна эта проверка (что она позволяет избежать)?
+			//Она гарантирует, что всегда будет рабочий список Records, и если он ещё не был создан, то создаётся новый список. И это помогает избежать NullReferenceException, лишней инициализации и неинцилиализированного состояния.
+			if (Records == null)
+				Records = new List<Data>(10);
+		}
+
+		private void OnDrawGizmos()
+		{
+			//todo comment: Зачем нужны эти проверки (что они позволляют избежать)?
+			//Проверяет, что список создан, и он не пустой. Они помогают избежать падения программы и ситуаций, когда в инспекторе поле списка не заполнено, а скрипт всё-равно пытается работать.
+			if (Records == null || Records.Count == 0) return;
+			var data = Records;
+			var prev = data[0].Position;
+			Gizmos.color = Color.green;
+			Gizmos.DrawWireSphere(prev, 0.3f);
+            //todo comment: Почему итерация начинается не с нулевого элемента?
+            //Потому что уже есть prev = data[0].Position. Если начать с i = 0, то получилось бы лишнее действие
+            for (int i = 1; i < data.Count; i++)
+			{
+				var curr = data[i].Position;
+				Gizmos.DrawWireSphere(curr, 0.3f);
+				Gizmos.DrawLine(prev, curr);
+				prev = curr;
+			}
+		}
+		
+#if UNITY_EDITOR
+		[ContextMenu("Create File")]
+		private void CreateFile()
+		{
+			//todo comment: Что происходит в этой строке?
+			//Создаёт новый файл по указанному пути. Если файл существует, то он будет очищен.
+			var stream = File.Create(Path.Combine(Application.dataPath, "Path.txt"));
+			//todo comment: Подумайте для чего нужна эта строка? (а потом проверьте догадку, закомментировав) 
+			//Закрывает поток к файлу, освобождая его для дальнейшей работы.
+			stream.Dispose();
+			UnityEditor.AssetDatabase.Refresh();
+			//В Unity можно искать объекты по их типу, для этого используется префикс "t:"
+			//После нахождения, Юнити возвращает массив гуидов (которые в мета-файлах задаются, например)
+			var guids = UnityEditor.AssetDatabase.FindAssets("t:TextAsset");
+			foreach (var guid in guids)
+			{
+				//Этой командой можно получить путь к ассету через его гуид
+				var path = UnityEditor.AssetDatabase.GUIDToAssetPath(guid);
+				//Этой командой можно загрузить сам ассет
+				var asset = UnityEditor.AssetDatabase.LoadAssetAtPath<TextAsset>(path);
+                //todo comment: Для чего нужны эти проверки?
+                //asset != null - проверка на то, что LoadAssetArtPath нашёл ассет по указанному пути. asset.name == "Path" - гарантия, что _json присвоен тот ассет, который нужен.
+                if (asset != null && asset.name == "Path")
+				{
+					_json = asset;
+					UnityEditor.EditorUtility.SetDirty(this);
+					UnityEditor.AssetDatabase.SaveAssets();
+					UnityEditor.AssetDatabase.Refresh();
+					//todo comment: Почему мы здесь выходим, а не продолжаем итерироваться?
+					//Если бы цикл продолжался, то _json мог быть перезаписан другим ассетом, что сломало бы логику.
+					return;
+				}
+			}
+		}
+
+		private void OnDestroy()
+		{
+			//todo logic...
+		}
+#endif
+	}
+}
